a0837e7d93e7053b6ecd7f9ece85d3124d217bc2
Aditya Yadav <yosefg7579@gmail.com>
2026-07-03 20:54:44 +0530

    Time: 0 ms (100%), Space: 8.6 MB (54.36%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -3,8 +3,7 @@ public:
     int reverse(int x) {
         long long rev=0;
         while (x!=0){
-        int rem = x%10;
-        rev = (rev*10)+rem;
+        rev = (rev*10)+x%10;
         if (rev >= INT_MAX || rev <= INT_MIN){
             return 0;
         }
