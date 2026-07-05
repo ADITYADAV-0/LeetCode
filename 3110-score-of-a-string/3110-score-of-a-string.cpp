be46b1f076051b14a38f220dce58c7e30605c8c3
Aditya Yadav <yosefg7579@gmail.com>
2026-07-05 13:34:44 +0530

    Time: 0 ms (100%), Space: 8.9 MB (65.59%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.cpp
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -3,7 +3,7 @@ public:
     int scoreOfString(string s) {
         int score = 0;
         for(int i = 0 ; i < s.size() - 1; i++){
-            score = score + abs(s[i] - s[i+1]);
+            score += abs(s[i] - s[i+1]);
         } 
         return score;
     }
